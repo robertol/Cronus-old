d9d63c27af578ec9d5247abadc7e3c14c8a33f7f
Carlos Heitor Lain <carlos.lain@virtuati.com.br>
2013-09-24 16:09:47 -0300

    Início das mudanças para multi linguagem.

---
+++ src/config/lang.h
@@ -0,0 +1,26 @@
+// Copyright (c) Cronus-Emulator Dev Team, licensed under GNU GPL.
+// See the LICENSE file
+// Portions Copyright (c) Athena Dev Teams
+#ifndef _CONFIG_LANGUAGE_H_
+#define _CONFIG_LANGUAGE_H_
+
+/**
+* Go to http://cronus-emulator.com for more information.
+*/
+
+/**
+* Enable this for translate the emulador to English
+*/
+//#define ce_english
+/**
+* Enable this for translate the emulador to Portuguese
+* NOTE: Default is active with Portuguese
+*/
+#define ce_portuguese
+/**
+* Enable this for translate the emulador to Spanish
+*/
+//#define ce_spanish
+
+
+#endif // _CONFIG_LANGUAGE_H_
